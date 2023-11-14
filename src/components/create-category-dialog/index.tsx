27a7b8fa1de8a7ff416347b76ce455974e314390
Agustinho Neto <agustinho.pneto@gmail.com>
2023-11-14 18:12:05 -0300

    feat(cateogories): add react hook form

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -1,21 +1,38 @@
 import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
+import { theme } from '../../styles/theme';
 import { Button } from '../button';
 import { Dialog } from '../dialog';
 import { Input } from '../input';
 import { Title } from '../title';
 import { Container } from './styles';
 
+type CreateCategoryData = {
+  title: string;
+  color: string;
+};
+
 export function CreateCategoryDialog() {
   const [open, setOpen] = useState(false);
+  const { register, handleSubmit } = useForm<CreateCategoryData>({
+    defaultValues: {
+      title: '',
+      color: theme.colors.primary,
+    },
+  });
 
   const handleClose = useCallback(() => {
     setOpen(false);
   }, []);
 
-  const onSubmit = useCallback(() => {
-    handleClose();
-  }, [handleClose]);
+  const onSubmit = useCallback(
+    (data: CreateCategoryData) => {
+      console.log(data);
+      handleClose();
+    },
+    [handleClose],
+  );
 
   return (
     <Dialog
@@ -29,18 +46,20 @@ export function CreateCategoryDialog() {
           subtitle="Crie uma nova categoria para suas transações"
         />
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <Input label="Nome" placeholder="Nome da categoria..." />
-            <Input label="Cor" type="color" />
+            <Input
+              label="Nome"
+              placeholder="Nome da categoria..."
+              {...register('title')}
+            />
+            <Input label="Cor" type="color" {...register('color')} />
           </div>
           <footer>
             <Button onClick={handleClose} variant="outline" type="button">
               Calcelar
             </Button>
-            <Button onClick={onSubmit} type="button">
-              Cadastrar
-            </Button>
+            <Button type="submit">Cadastrar</Button>
           </footer>
         </form>
       </Container>
