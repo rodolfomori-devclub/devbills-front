c5527c47c60634ade714287d841f5d79cd214040
Agustinho Neto <agustinho.pneto@gmail.com>
2023-11-14 15:03:02 -0300

    feat(transaction): add component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -6,7 +6,9 @@ import { Card } from '../../components/card';
 import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
+import { Transaction } from '../../components/transaction';
 import {
+  Aside,
   Balance,
   ChartAction,
   ChartContainer,
@@ -15,6 +17,7 @@ import {
   Header,
   InputGroup,
   Main,
+  SearchTransaction,
   Section,
 } from './styles';
 
@@ -88,6 +91,43 @@ export function Home() {
             <ChartContent></ChartContent>
           </ChartContainer>
         </Section>
+        <Aside>
+          <header>
+            <Title title="Transações" subtitle="Receitas e Gastos no período" />
+            <SearchTransaction>
+              <Input variant="black" placeholder="Procurar transação..." />
+              <ButtonIcon />
+            </SearchTransaction>
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#ff33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#ff33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#ff33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#ff33bb' }}
+              title="Mercado"
+            />
+          </header>
+        </Aside>
       </Main>
     </>
   );
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -77,3 +77,25 @@ export const ChartAction = styled.div`
   width: 8rem;
   gap: 0.5rem;
 `;
+
+export const Aside = styled.aside`
+  min-width: 22.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  padding: 1rem;
+  border-radius: 0.25rem;
+  background-color: ${theme.colors.dark};
+
+  header {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+export const SearchTransaction = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
