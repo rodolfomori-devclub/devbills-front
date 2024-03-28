3ad2fb12269a6589fb4919857a9c53a5267976f7
Agustinho Neto <agustinho.pneto@gmail.com>
2024-03-28 14:26:14 -0300

    add list transactions

--- src/validators/schemas.ts
+++ src/validators/schemas.ts
@@ -1,5 +1,20 @@
 import { z } from 'zod';
 
+export const transactionsFilterSchema = z.object({
+  title: z.string().optional(),
+  categoryId: z.string().optional(),
+  beginDate: z
+    .string()
+    .regex(/^(0[1-9]|[12][0-9]|3[01]\/0[0-9]|1[0-2]\/\d{4}$)/, {
+      message: 'Data inválida',
+    }),
+  endDate: z
+    .string()
+    .regex(/^(0[1-9]|[12][0-9]|3[01]\/0[0-9]|1[0-2]\/\d{4}$)/, {
+      message: 'Data inválida',
+    }),
+});
+
 export const createCategorySchema = z.object({
   title: z
     .string()
