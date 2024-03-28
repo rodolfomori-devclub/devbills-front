c112863a14ddd15c9ce85cf4186cfde63c1d2f4f
Agustinho Neto <agustinho.pneto@gmail.com>
2024-03-28 13:29:13 -0300

    add transactions form

--- src/components/create-transaction-dialog/index.tsx
+++ src/components/create-transaction-dialog/index.tsx
@@ -1,7 +1,12 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { InputMask } from '@react-input/mask';
+import dayjs from 'dayjs';
 import { useCallback, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { useFetchAPI } from '../../hooks/useFetchAPI';
+import { createTransactionSchema } from '../../validators/schemas';
+import { CreateTransactionData } from '../../validators/types';
 import { Button } from '../button';
 import { Dialog } from '../dialog';
 import { Input } from '../input';
@@ -10,6 +15,7 @@ import {
   Container,
   Content,
   CurrencyInput,
+  ErrorMessage,
   InputGroup,
   RadioForm,
   RadioGroup,
@@ -18,14 +24,30 @@ import {
 export function CreateTransactionDialog() {
   const { categories, fetchCategories } = useFetchAPI();
   const [open, setOpen] = useState(false);
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<CreateTransactionData>({
+    defaultValues: {
+      categoryId: 'null',
+      title: '',
+      amount: '',
+      date: dayjs('2024-01-01').format('DD/MM/YYYY'),
+      type: 'income',
+    },
+    resolver: zodResolver(createTransactionSchema),
+  });
 
   useEffect(() => {
     fetchCategories();
   }, [fetchCategories]);
 
   const handleClose = useCallback(() => {
+    reset();
     setOpen(false);
-  }, []);
+  }, [reset]);
 
   const onSubmit = useCallback(() => {
     handleClose();
@@ -43,11 +65,11 @@ export function CreateTransactionDialog() {
           subtitle="Crie uma nova transação para seu controle financeiro"
         />
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Content>
             <InputGroup>
               <label>Categoria</label>
-              <select>
+              <select {...register('categoryId')}>
                 <option value="null">Selecione uma categoria...</option>
                 {categories?.length &&
                   categories.map((item) => (
@@ -56,15 +78,27 @@ export function CreateTransactionDialog() {
                     </option>
                   ))}
               </select>
+              {errors.categoryId && (
+                <ErrorMessage>{errors.categoryId.message}</ErrorMessage>
+              )}
             </InputGroup>
-            <Input label="Nome" placeholder="Nome da transação..." />
+            <Input
+              label="Nome"
+              placeholder="Nome da transação..."
+              {...register('title')}
+              error={errors.title?.message}
+            />
             <InputGroup>
               <label>Valor</label>
               <CurrencyInput
                 placeholder="R$ 0,00"
                 format="currency"
                 currency="BRL"
+                {...register('amount')}
               />
+              {errors.amount && (
+                <ErrorMessage>{errors.amount.message}</ErrorMessage>
+              )}
             </InputGroup>
 
             <InputMask
@@ -74,17 +108,32 @@ export function CreateTransactionDialog() {
               label="Data"
               variant="black"
               placeholder="dd/mm/aaaa"
+              error={errors.date?.message}
+              {...register('date')}
             />
 
             <RadioForm>
               <RadioGroup>
-                <input type="radio" id="income" value="income" name="type" />
+                <input
+                  type="radio"
+                  id="income"
+                  value="income"
+                  {...register('type')}
+                />
                 <label htmlFor="income">Receita</label>
               </RadioGroup>
               <RadioGroup>
-                <input type="radio" id="expense" value="expense" name="type" />
+                <input
+                  type="radio"
+                  id="expense"
+                  value="expense"
+                  {...register('type')}
+                />
                 <label htmlFor="expense">Gasto</label>
               </RadioGroup>
+              {errors.type && (
+                <ErrorMessage>{errors.type.message}</ErrorMessage>
+              )}
             </RadioForm>
           </Content>
 
@@ -92,9 +141,7 @@ export function CreateTransactionDialog() {
             <Button onClick={handleClose} variant="outline" type="button">
               Calcelar
             </Button>
-            <Button onClick={onSubmit} type="button">
-              Cadastrar
-            </Button>
+            <Button type="submit">Cadastrar</Button>
           </footer>
         </form>
       </Container>
--- src/validators/schemas.ts
+++ src/validators/schemas.ts
@@ -9,3 +9,23 @@ export const createCategorySchema = z.object({
     .string()
     .regex(/^#[A-Fa-f0-9]{6}$/, { message: 'Deve seguir o padrão #rrggbb' }),
 });
+
+export const createTransactionSchema = z.object({
+  categoryId: z
+    .string()
+    .regex(/^(?!null$)/g, { message: 'Escolha uma categoria' }),
+  title: z
+    .string()
+    .min(1, { message: 'Deve conter pelo menos 1 caractere' })
+    .max(255),
+  amount: z
+    .string()
+    .min(1, { message: 'Deve conter pelo menos 1 dígito' })
+    .max(255),
+  date: z.string().regex(/^(0[1-9]|[12][0-9]|3[01]\/0[0-9]|1[0-2]\/\d{4}$)/, {
+    message: 'Data inválida',
+  }),
+  type: z.enum(['income', 'expense'], {
+    errorMap: () => ({ message: 'Selecione um tipo válido' }),
+  }),
+});
